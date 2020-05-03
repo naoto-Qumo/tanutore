8fd21b43b4401f3bd712b296cb6a1d1e1cf66680
keiya2-Qumo <seayamadao3@gmail.com>
2020-05-03 23:50:34 +0900

    2020/05/03 交換登録画面

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ $(function(){
             $('.repass-error-msgArea').hide();
         }
     });
-
+    
     // フッターがコンテンツが少ない場合でも画面最下部に表示されるようにする
     $footer = $('#footer');
     if(window.innerHeight > $footer.offset().top + $footer.outerHeight()){
